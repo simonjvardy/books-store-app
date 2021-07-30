b1622a9230db8aecf425c206f9c6a8e81dc56a90
simonjvardy <65361514+simonjvardy@users.noreply.github.com>
2021-07-30 11:03:35 +0100

    Dev: Add class docstring and reorder the methods

--- backend.py
+++ backend.py
@@ -1,6 +1,32 @@
 import sqlite3
 
 class Database:
+    """
+    NAME
+        Database
+    DESCRIPTION
+        This class provides access to the database methods to open and
+        close connections to the database and CRUD functionality for the
+        user to interact with the database.
+    METHODS
+        insert(title,author,year,isbn)
+            Write a row of data containing the 4 arguments relating to
+            the database table fields.
+        
+        view()
+            Read / return all database table rows
+        
+        search(title,author,year,isbn)
+            Read / return all matching database table rows where the 
+            supplied arguments are true
+        
+        update(id,title, author, year, isbn)
+            Update the selected database table row with data passed
+            as the 4 arguments: title, author, year, isbn
+        
+        delete(id)
+            Delete the selected row from the database table. Not recoverable.  
+    """
 
     def __init__(self, db):
         """
@@ -21,7 +47,6 @@ class Database:
         )
         self.conn.commit()
 
-
     def insert(self,title,author,year,isbn):
         """
         Method to create new rows in the database
@@ -32,7 +57,6 @@ class Database:
             """, (title, author, year, isbn)
         )
         self.conn.commit()
-
 
     def view(self):
         """
@@ -46,7 +70,6 @@ class Database:
         )
         rows = self.cur.fetchall()
         return rows
-
 
     def search(self,title="",author="",year="",isbn=""):
         """
@@ -65,19 +88,6 @@ class Database:
         rows = self.cur.fetchall()
         return rows
 
-    def delete(self,id):
-        """
-        Method to delete a row in the database
-        """
-        self.cur.execute(
-            """
-            DELETE FROM book
-            WHERE id = ?
-            """, (id,)
-        )
-        self.conn.commit()
-
-
     def update(self,id,title, author, year, isbn):
         """
         Method to update a row in the database
@@ -91,6 +101,18 @@ class Database:
             isbn = ?
             WHERE id = ?
             """, (title, author, year, isbn, id)
+        )
+        self.conn.commit()
+
+    def delete(self,id):
+        """
+        Method to delete a row in the database
+        """
+        self.cur.execute(
+            """
+            DELETE FROM book
+            WHERE id = ?
+            """, (id,)
         )
         self.conn.commit()
 
